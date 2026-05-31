f307bf743ef63cc087e19b508ee00c2bf3d71020
kumarkoppala <kkoppala241@gmail.com>
2026-05-31 07:06:28 +0530

    initial

--- Roboshop-automation-with-shell=script/Catelogue.sh
+++ Roboshop-automation-with-shell=script/Catelogue.sh
@@ -101,7 +101,7 @@ echo "Registering MongoDB repository infrastructure..." | tee -a "$logs"
 cat <<EOF > /etc/yum.repos.d/mongo.repo
 [mongodb-org-7.0]
 name=MongoDB Repository
-baseurl=https://mongodb.org
+baseurl=baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
 enabled=1
 gpgcheck=0
 EOF
